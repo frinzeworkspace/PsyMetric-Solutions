3adbb3ac98695439481c51b6d60937f19485e4cf
Frinze <codewithfrinze@gmail.com>
2024-08-25 08:42:52 +0800

    update

--- scripts/records.js
+++ scripts/records.js
@@ -593,3 +593,35 @@ const records = {
         }
     }
 }
+
+
+
+
+
+
+
+
+
+
+document.getElementById('burger-icon').addEventListener('click', function() {
+    const sidebar = document.getElementById('sidebar');
+    const menuContainer = document.getElementById('menu-container');
+
+    sidebar.classList.toggle('expanded');
+});
+
+
+
+
+
+
+window.addEventListener('load', function() {
+   
+    setTimeout(function() {
+        document.getElementById('preloader').style.opacity = '0';
+        setTimeout(function() {
+            document.getElementById('preloader').style.display = 'none';
+            document.getElementById('content').style.display = 'block';
+        }, 1000); 
+    }, 200); 
+});
